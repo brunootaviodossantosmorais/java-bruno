191833566e9cf7edf4f901da61f0121be3ca9c13
brunootaviodossantosmorais <bruno.santos.morais@escola.pr.gov.br>
2024-08-12 16:46:12 -0300

    adicionando arquivo js css

--- sketch.js
+++ sketch.js
@@ -1,16 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.4/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.4/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
-
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  <script src="p5.collide2d.js"></script>
-  </body>
-</html>
+function setup() {
+    createCanvas(600, 600);
+     background("red");
+  }
+  
+  
+  function draw() {
+    
+    stroke ("orange");
+    fill ("black");
+   
+  let posicaoHorizontal; // x
+  let posicaoVertical; // y
+  
+  function setup() {
+    createCanvas(400, 400);
+     background(color(100,0,0));
+    cor = color(random(0,255), random(0,255), random(0,255));
+    posicaoHorizontal = 200;
+    posicaoVertical = 200;
+  }
+  
+  
+  function draw() {
+    
+    fill(cor);
+    circle(posicaoHorizontal,posicaoVertical,50);
+   
+    
+    
+    if (mouseX < posicaoHorizontal){
+      posicaoHorizontal =  posicaoHorizontal - 1;
+    }
+    
+    if (mouseX > posicaoHorizontal){
+      posicaoHorizontal =  posicaoHorizontal + 1;
+    }
+    
+    if (mouseY < posicaoVertical){
+      posicaoVertical--;
+    }
+      
+    if (mouseY > posicaoVertical){
+      posicaoVertical++;
+    }
+    
+    if (mouseIsPressed){
+      cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+    }
+      
+  }
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
